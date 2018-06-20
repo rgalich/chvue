19684f069d516ecbd3de43fc33f813930455ab36
remi <remigalichon@gmail.com>
2018-06-20 23:24:39 +0200

    init

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="dashboard">
+    <v-container fluid fill-height grid-list-xl>
+      <v-layout row wrap>
+        <v-flex md4>
+          <v-btn block>Gestion des recettes</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,41 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <v-app id="inspire">
+      <v-navigation-drawer
+        fixed
+        v-model="drawer"
+        app
+      >
+        <v-list dense>
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-icon>home</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Home</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-icon>contact_mail</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Contact</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar color="indigo" dark fixed app>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title>Application</v-toolbar-title>
+      </v-toolbar>
+      <v-content>
+        <router-view></router-view>
+      </v-content>
+      <v-footer color="indigo" app inset>
+        <span class="white--text">&copy; 2017</span>
+      </v-footer>
+    </v-app>
   </div>
 </template>
 
@@ -14,5 +48,7 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  private drawer: boolean = false;
+}
 </script>
